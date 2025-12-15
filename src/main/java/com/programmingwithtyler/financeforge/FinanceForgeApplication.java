1a1ee96cdd5bb414226fa135025f82d47cc93309
ProgrammingWithTyler <114627665+ProgrammingWithTyler@users.noreply.github.com>
2025-12-15 16:58:48 -0600

    Squashed commit of the following:
    commit 9a1c9891a274ba85d3058982b4ca5f851466e7a0
    Author: ProgrammingWithTyler <114627665+ProgrammingWithTyler@users.noreply.github.com>
    Date:   Mon Dec 15 16:51:47 2025 -0600
    
        feat(domain): add Account entity (#2)
    
        - Defined Account domain model
        - Added AccountType enum
        - Included audit timestamps and balance fields
        - Updated documentation
        - Added sample Controller
        - Updated configuration files
        - remove .gradle and .idea from repository

---
+++ src/main/java/com/programmingwithtyler/financeforge/FinanceForgeApplication.java
@@ -0,0 +1,13 @@
+package com.programmingwithtyler.financeforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceForgeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/programmingwithtyler/financeforge/domain/AccountType.java
@@ -0,0 +1,5 @@
+package com.programmingwithtyler.financeforge.domain;
+
+public enum AccountType {
+    CHECKING, SAVINGS, CREDIT_CARD, INVESTMENT, CASH
+}
